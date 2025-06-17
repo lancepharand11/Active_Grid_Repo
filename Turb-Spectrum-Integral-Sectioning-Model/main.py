d9ccf6ef2a3283d48c7911429de2d976977fd190
ctops4 <ctoppings@uwaterloo.ca>
2025-06-17 12:42:30 -0400

    Fixed device error on Windows
    Also added automatic sample number calculation and timestamp methods to turbulence parameters class.

--- Turb-Spectrum-Integral-Sectioning-Model/main.py
+++ Turb-Spectrum-Integral-Sectioning-Model/main.py
@@ -27,7 +27,7 @@ criterion = nn.MSELoss()
 
 # Load the saved model weights
 model_path = "./turb_spectrum_integral_section_model.pth"
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load(model_path,map_location=device))
 model.eval()  # Set the model to evaluation mode
 
 print("Model successfully loaded and ready for inference.")
