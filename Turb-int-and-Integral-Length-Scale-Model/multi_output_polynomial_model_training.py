9d881c8d9e5106579a82e35d257628ab8103aba0
ctops4 <ctoppings@uwaterloo.ca>
2025-06-24 15:43:14 -0400

    Custom proportion of training data
    uses shuffle-split instead of kfold
    created a polynomial model

--- Turb-int-and-Integral-Length-Scale-Model/multi_output_polynomial_model_training.py
+++ Turb-int-and-Integral-Length-Scale-Model/multi_output_polynomial_model_training.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 24 14:05:25 2025
+
+@author: Connor
+"""
+
 # Multi Output Neural Network Model for Active Grid
 # Author: Lance Pharand, 2025
 # NOTEs:
@@ -17,11 +24,13 @@ sys.path.insert(0, os.path.abspath('../'))
 from Turbulence_Parameters_class import Turbulence_Parameters
 from pathlib import Path
 import joblib
-from torch.utils.data import DataLoader, TensorDataset
 import matplotlib.pyplot as plt
 from sklearn.model_selection import ShuffleSplit
 from sklearn.preprocessing import MinMaxScaler
-from torch import nn, optim
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+from torch import nn
 import torch
 import copy
 
@@ -94,43 +103,32 @@ Y_Uncertainty = IO_data.iloc[:, 6:8]
 X_all = torch.tensor(X.values, dtype=torch.float32)
 Y_all = torch.tensor(Y.values, dtype=torch.float32)
 ###################################################################
-## K-Fold CV Setup
+## Shuffle-Split CV Setup
 ###################################################################
 n_models = 10
-kf = ShuffleSplit(n_splits=n_models, test_size=0.90, random_state=0)  # NOTE: no seed used
+SS = ShuffleSplit(n_splits=n_models, test_size=0.80, random_state=0)  # NOTE: no seed used
 
-input_size, output_size = X_all.shape[1], Y_all.shape[1]
-hidden_size = 64
-num_epochs = 1000
-learning_rate = 1e-3
-batch_size = 16
 device = torch.device("cuda" if torch.cuda.is_available() else ("mps" if torch.backends.mps.is_available() else "cpu"))
 
 
 def get_model():
-    return nn.Sequential(nn.Linear(input_size, hidden_size),
-                         nn.BatchNorm1d(hidden_size),
-                         nn.LeakyReLU(),
-                         nn.Linear(hidden_size, hidden_size // 2),
-                         nn.BatchNorm1d(hidden_size // 2),
-                         nn.LeakyReLU(),
-                         nn.Linear(hidden_size // 2, output_size)
-                         ).to(device)
-
+    model = Pipeline([('poly', PolynomialFeatures(degree=5)), 
+                      ('linear', LinearRegression(fit_intercept=False))])
+    return model
 
 ###################################################################
-## K-Fold based training Loop
+## Shuffle-Split based training Loop
 ###################################################################
 fold_results = []
 best_overall_rmse = np.inf
-best_overall_weights = None
+best_overall_model = None
 best_norm_rmse_turb_int = None
 best_norm_rmse_L_ux = None
 best_scaler_x = None
 best_scaler_y = None
 fraction_acceptable = np.zeros(n_models)
 
-for fold, (train_idx, val_idx) in enumerate(kf.split(X_all)):
+for fold, (train_idx, val_idx) in enumerate(SS.split(X_all)):
     print(f"\nFold {fold + 1}")
     scaler_x = MinMaxScaler(feature_range=(-1, 1))
     scaler_y = MinMaxScaler(feature_range=(-1, 1))
@@ -139,6 +137,7 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X_all)):
     y_train = scaler_y.fit_transform(Y_all[train_idx])
     x_val = scaler_x.transform(X_all[val_idx])
     y_val = scaler_y.transform(Y_all[val_idx])
+    
     y_val_uncertainty = Y_Uncertainty.iloc[val_idx]
 
     x_train = torch.tensor(x_train, dtype=torch.float32).to(device)
@@ -146,126 +145,91 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X_all)):
     x_val = torch.tensor(x_val, dtype=torch.float32).to(device)
     y_val = torch.tensor(y_val, dtype=torch.float32).to(device)
 
-    train_dataset = TensorDataset(x_train, y_train)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
     model = get_model()
-    training_crit = nn.SmoothL1Loss()
     mse_crit = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
-
+    
     best_rmse, best_epoch = np.inf, -1
     best_weights = None
+    
+    model.fit(x_train,y_train)
 
-    train_rmse_curve = []
-    val_rmse_curve = []
+    y_train_pred = model.predict(x_train)
+    y_val_pred = model.predict(x_val)
 
-    for epoch in range(num_epochs):
-        model.train()
-        for xb, yb in train_loader:
-            optimizer.zero_grad()
-            y_pred = model(xb)
-            loss = training_crit(y_pred, yb)
-            loss.backward()
-            optimizer.step()
+    y_train_pred_unscaled = torch.tensor(scaler_y.inverse_transform(y_train_pred))
+    y_val_pred_unscaled = torch.tensor(scaler_y.inverse_transform(y_val_pred))
 
-        model.eval()
-        with torch.no_grad():
-            y_train_pred = model(x_train)
-            y_val_pred = model(x_val)
+    y_train_unscaled = torch.tensor(Y_all[train_idx].numpy())
+    y_val_unscaled = torch.tensor(Y_all[val_idx].numpy())
 
-            y_train_pred_unscaled = torch.tensor(scaler_y.inverse_transform(y_train_pred.cpu().numpy()))
-            y_val_pred_unscaled = torch.tensor(scaler_y.inverse_transform(y_val_pred.cpu().numpy()))
+    train_rmse = torch.sqrt(mse_crit(y_train_pred_unscaled, y_train_unscaled)).item()
+    val_rmse = torch.sqrt(mse_crit(y_val_pred_unscaled, y_val_unscaled)).item()
 
-            y_train_unscaled = torch.tensor(Y_all[train_idx].numpy())
-            y_val_unscaled = torch.tensor(Y_all[val_idx].numpy())
+    if val_rmse < best_rmse:
+        best_rmse = val_rmse
+        best_weights = copy.deepcopy(model)
 
-            train_rmse = torch.sqrt(mse_crit(y_train_pred_unscaled, y_train_unscaled)).item()
-            val_rmse = torch.sqrt(mse_crit(y_val_pred_unscaled, y_val_unscaled)).item()
-
-            train_rmse_curve.append(train_rmse)
-            val_rmse_curve.append(val_rmse)
-
-            if val_rmse < best_rmse:
-                best_rmse = val_rmse
-                best_epoch = epoch
-                best_weights = copy.deepcopy(model.state_dict())
-
-    print(f"Best Epoch: {best_epoch}, Best RMSE (unscaled): {best_rmse:.4f}")
+    print(f" RMSE (unscaled): {best_rmse:.4f}")
     fold_results.append(best_rmse)
-
-    #
-    # Learning curve
-    #
-    plt.figure(figsize=(10, 8))
-    plt.plot(train_rmse_curve, label="Train RMSE (unscaled)")
-    plt.plot(val_rmse_curve, label="Val RMSE (unscaled)")
-    plt.xlabel("Epoch")
-    plt.ylabel("RMSE")
-    plt.title(f"Learning Curve - Fold {fold+1}")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
 
     #
     # Residuals plot and compare against measurement uncertainty
     #
-    model.load_state_dict(best_weights)
-    model.eval()
-    with torch.no_grad():
-        y_val_pred = model(x_val)
-        y_val_pred_unscaled_np = scaler_y.inverse_transform(y_val_pred.cpu().numpy())
-        y_val_unscaled_np = Y_all[val_idx].numpy()
-        residuals = y_val_unscaled_np - y_val_pred_unscaled_np
-        
-        # How many predictions are within measurement uncertainty?
-        within_Uncertainty = residuals < y_val_uncertainty.to_numpy()
-        fraction_acceptable[fold] = within_Uncertainty.sum() / within_Uncertainty.size       
+    model = best_weights
+   
+    y_val_pred = model.predict(x_val)
+    y_val_pred_unscaled_np = scaler_y.inverse_transform(y_val_pred)
+    y_val_unscaled_np = Y_all[val_idx].numpy()
+    residuals = y_val_unscaled_np - y_val_pred_unscaled_np
+    
+    # How many predictions are within measurement uncertainty?
+    within_Uncertainty = residuals < y_val_uncertainty.to_numpy()
+    fraction_acceptable[fold] = within_Uncertainty.sum() / within_Uncertainty.size       
 
-        # Compute per output RMSE
-        rmse_turb_int = torch.sqrt(torch.tensor(mse_crit(torch.tensor(y_val_pred_unscaled_np[:, 0]),
-                                                          torch.tensor(y_val_unscaled_np[:, 0])))
-                                   ).item()
-        rmse_L_ux = torch.sqrt(torch.tensor(mse_crit(torch.tensor(y_val_pred_unscaled_np[:, 1]),
-                                                      torch.tensor(y_val_unscaled_np[:, 1])))
+    # Compute per output RMSE
+    rmse_turb_int = torch.sqrt(torch.tensor(mse_crit(torch.tensor(y_val_pred_unscaled_np[:, 0]),
+                                                      torch.tensor(y_val_unscaled_np[:, 0])))
                                ).item()
+    rmse_L_ux = torch.sqrt(torch.tensor(mse_crit(torch.tensor(y_val_pred_unscaled_np[:, 1]),
+                                                  torch.tensor(y_val_unscaled_np[:, 1])))
+                           ).item()
 
-        # Normalize based on range
-        range_turb_int = Y["Turbulence Intensity"].max() - Y["Turbulence Intensity"].min()
-        range_L_ux = Y["L_ux / M"].max() - Y["L_ux / M"].min()
-        norm_rmse_turb_int = rmse_turb_int / range_turb_int
-        norm_rmse_L_ux = rmse_L_ux / range_L_ux
+    # Normalize based on range
+    range_turb_int = Y["Turbulence Intensity"].max() - Y["Turbulence Intensity"].min()
+    range_L_ux = Y["L_ux / M"].max() - Y["L_ux / M"].min()
+    norm_rmse_turb_int = rmse_turb_int / range_turb_int
+    norm_rmse_L_ux = rmse_L_ux / range_L_ux
 
-        print(f"Fold {fold + 1} Normalized RMSEs:")
-        print(f"    Turbulence Intensity: {norm_rmse_turb_int:.4f}")
-        print(f"    L_ux / M: {norm_rmse_L_ux:.4f}")
+    print(f"Fold {fold + 1} Normalized RMSEs:")
+    print(f"    Turbulence Intensity: {norm_rmse_turb_int:.4f}")
+    print(f"    L_ux / M: {norm_rmse_L_ux:.4f}")
 
-        temp_inputs = X_all[val_idx].detach().numpy()
+    temp_inputs = X_all[val_idx].detach().numpy()
 
-        for i, target_name in enumerate(["Turbulence Intensity", "L_ux / M"]):
-            # plt.figure(figsize=(10, 8))
-            # plt.scatter(y_val_pred_unscaled_np[:, i], residuals[:, i], alpha=0.7, label=f"Residuals for {target_name}")
-            # plt.axhline(0, color="red", linestyle="--", linewidth=1.5, label="Zero Residual Line")
-            # plt.xlabel(f"Predicted {target_name} (unscaled)")
-            # plt.ylabel(f"Residual {target_name} (unscaled)")
-            # plt.title(f"Fold {fold + 1} Residual Plot: {target_name}")
-            # plt.legend()
-            # plt.grid(True)
-            # plt.show()
+    for i, target_name in enumerate(["Turbulence Intensity", "L_ux / M"]):
+        # plt.figure(figsize=(10, 8))
+        # plt.scatter(y_val_pred_unscaled_np[:, i], residuals[:, i], alpha=0.7, label=f"Residuals for {target_name}")
+        # plt.axhline(0, color="red", linestyle="--", linewidth=1.5, label="Zero Residual Line")
+        # plt.xlabel(f"Predicted {target_name} (unscaled)")
+        # plt.ylabel(f"Residual {target_name} (unscaled)")
+        # plt.title(f"Fold {fold + 1} Residual Plot: {target_name}")
+        # plt.legend()
+        # plt.grid(True)
+        # plt.show()
 
-            fig1 = plt.figure(figsize=(10, 8))
-            ax1 = fig1.add_subplot(111, projection='3d')
-            p1 = ax1.scatter(temp_inputs[:, 0], temp_inputs[:, 1], temp_inputs[:, 2],
-                             c=residuals[:, i], cmap='magma',
-                             marker='o', s=50, alpha=0.8
-                             )
-            cbar1 = fig1.colorbar(p1, ax=ax1, shrink=0.5, pad=0.1)
-            cbar1.set_label('Residuals - ' + target_name)
-            ax1.set_xlabel('Grid Re', labelpad=7)
-            ax1.set_ylabel('Rossby Number')
-            ax1.set_zlabel('Shaft Speed Std Dev * M / u_inf', labelpad=8, rotation=0)
-            ax1.set_title('3D Scatter: ' + target_name)
-            plt.show()
+        fig1 = plt.figure(figsize=(10, 8))
+        ax1 = fig1.add_subplot(111, projection='3d')
+        p1 = ax1.scatter(temp_inputs[:, 0], temp_inputs[:, 1], temp_inputs[:, 2],
+                         c=residuals[:, i], cmap='magma',
+                         marker='o', s=50, alpha=0.8
+                         )
+        cbar1 = fig1.colorbar(p1, ax=ax1, shrink=0.5, pad=0.1)
+        cbar1.set_label('Residuals - ' + target_name)
+        ax1.set_xlabel('Grid Re', labelpad=7)
+        ax1.set_ylabel('Rossby Number')
+        ax1.set_zlabel('Shaft Speed Std Dev * M / u_inf', labelpad=8, rotation=0)
+        ax1.set_title('3D Scatter: ' + target_name)
+        plt.show()
 
     # Track best model across all folds
     if best_rmse < best_overall_rmse:
@@ -273,7 +237,7 @@ for fold, (train_idx, val_idx) in enumerate(kf.split(X_all)):
         best_overall_train_idx, best_overall_val_idx = train_idx, val_idx
         best_norm_rmse_turb_int = norm_rmse_turb_int
         best_norm_rmse_L_ux = norm_rmse_L_ux
-        best_overall_weights = copy.deepcopy(best_weights)
+        best_overall_model = copy.deepcopy(best_weights)
         best_scaler_x = copy.deepcopy(scaler_x)
         best_scaler_y = copy.deepcopy(scaler_y)
 
@@ -294,7 +258,7 @@ scaler_x_fname = os.path.join(out_dir, f"scaler_x_{unique_id}.pkl")
 scaler_y_fname = os.path.join(out_dir, f"scaler_y_{unique_id}.pkl")
 log_fname = os.path.join(out_dir, "rmse_results.txt")
 
-torch.save(best_overall_weights, model_fname)
+torch.save(best_overall_model, model_fname)
 joblib.dump(best_scaler_x, scaler_x_fname)
 joblib.dump(best_scaler_y, scaler_y_fname)
 
